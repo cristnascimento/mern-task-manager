16f48e60b9c4d0cbc0d38865b70b1a8cf9058a9f
Cristiano Nascimento <cristnascimento@gmail.com>
2020-08-04 22:20:55 -0300

    Checking the routes

--- blog-app/src/app.js
+++ blog-app/src/app.js
@@ -1,6 +1,17 @@
 const http = require('http')
 
 const server = http.createServer( (req, res) => {
+    if (req.url === '/about')
+        res.end('The about page')
+    else if (req.url === '/contact')
+        res.end('The contact page')
+    else if (req.url === '/')
+        res.end('The Home Page')
+    else {
+        res.writeHead(404)
+        res.end('Not found')
+    }
+
     console.log(req.url)
     res.end('Hello Node.js')
 })
