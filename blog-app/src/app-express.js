9d204c90ede24a990962d0712aeb8a3ff0e2b831
Cristiano Nascimento <cristnascimento@gmail.com>
2020-08-06 22:18:43 -0300

    Using routes and sending file as response

--- blog-app/src/app-express.js
+++ blog-app/src/app-express.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
 app.listen(3000, () => {
@@ -6,5 +7,14 @@ app.listen(3000, () => {
 })
 
 app.get('/', (req, res) => {
-    res.json({name: 'Greg Lim'})
+    res.sendFile(path.resolve(__dirname, 'index.html'))
+    //res.json({name: 'Greg Lim'})
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'about.html'))
+})
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'contact.html'))
 })
