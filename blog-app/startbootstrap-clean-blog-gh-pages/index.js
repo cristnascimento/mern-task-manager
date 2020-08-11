465f55e962aee75dc3aac16c706ba0151e1e71e3
Cristiano Nascimento <cristnascimento@gmail.com>
2020-08-10 21:20:56 -0300

    Adding EJS template engine

--- blog-app/startbootstrap-clean-blog-gh-pages/index.js
+++ blog-app/startbootstrap-clean-blog-gh-pages/index.js
@@ -1,27 +1,29 @@
 const express = require('express')
 const path = require('path')
+const ejs = require('ejs')
 
 const app = express()
 
+app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.listen(4000, () => {
     console.log('Listening on port 4000')
 })
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+    res.render('index')
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+    res.render('about')
 })
 
 app.get('/contact', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+    res.render('contact')
 })
 
 app.get('/post', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+    res.render('post')
 })
 
 
