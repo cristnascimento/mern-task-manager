ca51171254e7c577c05b0d5c2808a45b7526beee
Cristiano Nascimento <cristnascimento@gmail.com>
2020-08-14 21:20:31 -0300

    Showing blog in his own page.

--- blog-app/startbootstrap-clean-blog-gh-pages/index.js
+++ blog-app/startbootstrap-clean-blog-gh-pages/index.js
@@ -31,8 +31,9 @@ app.get('/contact', (req, res) => {
     res.render('contact')
 })
 
-app.get('/post', (req, res) => {
-    res.render('post')
+app.get('/post/:id', async (req, res) => {
+    const blogpost = await BlogPost.findById(req.params.id);
+    res.render('post', {blogpost});
 })
 
 app.get('/posts/new', (req, res) => {
