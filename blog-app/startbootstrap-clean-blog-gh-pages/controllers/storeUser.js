34ac812a874f1843de136fb490b2a5d95223bddd
Cristiano Nascimento <cristnascimento@gmail.com>
2020-08-29 22:02:21 -0300

    Showing errors on form.

--- blog-app/startbootstrap-clean-blog-gh-pages/controllers/storeUser.js
+++ blog-app/startbootstrap-clean-blog-gh-pages/controllers/storeUser.js
@@ -4,7 +4,8 @@ const path = require('path');
 module.exports = (req, res) => {
     User.create(req.body, (error, user) => {
         if (error) {
-            console.log(error);
+            const validationErrors = Object.keys(error.errors).map(key => error.errors[key].message);
+            req.session.validationErrors = validationErrors;
             return res.redirect('/auth/register');
         }
         res.redirect("/");
