2798aaf9dc3015866be17a60bc25f329d1a84c62
Cristiano Nascimento <cristnascimento@gmail.com>
2020-09-02 21:32:57 -0300

    Persisting request data on form after error.

--- blog-app/startbootstrap-clean-blog-gh-pages/controllers/newUser.js
+++ blog-app/startbootstrap-clean-blog-gh-pages/controllers/newUser.js
@@ -1,5 +1,20 @@
 const flash = require('connect-flash');
 
 module.exports = (req, res) => {
-    res.render('register', {errors: req.flash('validationErrors')});
+    let username = '';
+    let password = '';
+    // after this flash is undefined
+    let data = req.flash('data')[0];
+
+    if (typeof data != "undefined") {
+        username = data.username;
+        password = data.password;
+        console.log("not undefined");
+    }
+
+    res.render('register', {
+        errors: req.flash('validationErrors'),
+        username: username,
+        password: password
+    });
 }
